25af206678466b0c31bac82c678dec1dfb24a291
Jennie <jguil429@gmail.com>
2024-03-14 17:00:11 -0500

    debugging date format

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync');
 const Plant = require('./models/plant');
 const methodOverride = require('method-override');
 
@@ -26,41 +27,46 @@ app.get('/', (req, res) => {
     res.render('home')
 });
 
-app.get('/plants', async(req, res) => {
+app.get('/plants', catchAsync(async(req, res) => {
     const plants = await Plant.find({});
     res.render('plants/library', {plants});
-});
+}));
 
 app.get('/plants/new', (req, res) => {
     res.render('plants/new');
 });
 
-app.post('/plants', async (req, res) => {
+app.post('/plants', catchAsync(async (req, res) => {
     const plant = new Plant(req.body.plant);
     await plant.save();
     res.redirect(`/plants/${plant._id}`);
-});
+}));
 
-app.get('/plants/:id', async(req, res) => {
+app.get('/plants/:id', catchAsync(async(req, res) => {
     const plant = await Plant.findById(req.params.id);
     res.render('plants/show', { plant });
-});
+}));
 
-app.get('/plants/:id/edit', async(req, res) => {
+app.get('/plants/:id/edit', catchAsync(async(req, res) => {
     const plant = await Plant.findById(req.params.id);
     res.render('plants/edit', { plant });
-});
+}));
 
-app.put('/plants/:id', async (req, res) => {
+app.put('/plants/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const plant = await Plant.findByIdAndUpdate(id, {...req.body.plant});
     res.redirect(`/plants/${plant._id}`);
-});
+}));
 
-app.delete('/plants/:id', async (req, res) => {
+app.delete('/plants/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Plant.findByIdAndDelete(id);
     res.redirect('/plants');
+}));
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    res.send("Problem Detected.")
 })
 
 app.listen(3000, () => {
