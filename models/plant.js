5dca67b291bfac58b8da028df0769703afa174bc
Jordan Steel <jordansteel@randallreilly.com>
2024-04-11 17:32:34 -0500

    fix show and library image sizes

--- models/plant.js
+++ models/plant.js
@@ -11,6 +11,10 @@ ImageSchema.virtual('thumbnail').get(function() {
     return this.url.replace('/upload', '/upload/c_auto,g_auto,h_380,w_490');
 });
 
+ImageSchema.virtual('library_card').get(function() {
+    return this.url.replace('/upload', '/upload/c_auto,g_auto,h_275,w_360');
+});
+
 const PlantSchema = new Schema({
     common_name: String,
     scientific_name: String,
--- seeds/index.js
+++ seeds/index.js
@@ -23,6 +23,14 @@ const seedDB = async() => {
     });
     await testUser.save();
 
+    const seedImages = [
+        "https://res.cloudinary.com/dpblveo9k/image/upload/v1712788124/LeafLibrary/btakq1nacknwalruc8qx.jpg",
+        "https://res.cloudinary.com/dpblveo9k/image/upload/v1712788124/LeafLibrary/efuwplf5f9gphcl0guao.jpg",
+        "https://res.cloudinary.com/dpblveo9k/image/upload/v1712583766/LeafLibrary/dvukzxwzqd7f8cm1u2yn.jpg",
+        "https://res.cloudinary.com/dpblveo9k/image/upload/v1712582502/LeafLibrary/tztbyiusbhfljtkv8fxd.jpg",
+        "https://res.cloudinary.com/dpblveo9k/image/upload/v1712245923/LeafLibrary/wdgg5oe7bvo7yzqfabs7.jpg"
+    ]
+
     const resize = 'c_auto,g_auto,h_380,w_490'
 
     for (let i = 0; i < 5; i++) {
@@ -33,10 +41,10 @@ const seedDB = async() => {
             duration: 'perennial',
             images: [
               {
-                url: `https://res.cloudinary.com/dpblveo9k/image/upload/${resize}/v1712770816/LeafLibrary/dveiqux0gflqcvk8nphp.jpg`,
-                filename: 'LeafLibrary/dveiqux0gflqcvk8nphp'
+                url: seedImages[i].replace('upload/', 'upload/c_auto,g_auto,h_380,w_490/'),
+                filename: `LeafLibrary_${i}`
               }
-              ]
+            ]
         }) 
         await p.save();
     }
