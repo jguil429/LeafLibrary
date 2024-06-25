e1281e7270c388ca9ae2596fcf03023d811ae252
Jordan Steel <jordansteel@randallreilly.com>
2024-06-24 20:17:13 -0500

    adjust DB connection in seed file

--- seeds/index.js
+++ seeds/index.js
@@ -86,14 +86,11 @@ const User = require('../models/user');
 const Image = require('../models/image');
 const { DateTime } = require("luxon");
 
-const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/plantlib';
+const mongoURI = process.env.DB_URL || 'mongodb://localhost:27017/plantlib';
 
 async function connectDB() {
     try {
-        await mongoose.connect(mongoURI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(mongoURI);
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
